1167c8f07ab3d2fc77991507e19c93ae18cba3b7
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2018-12-10 23:39:26 +0000

    Initial commit

---
+++ Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "item1", "item2" };
+        }
+    }
+}
